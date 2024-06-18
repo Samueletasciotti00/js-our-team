745248cd59ae9737c25b594cd73e36985f57d889
Samuele <samueletasciotti@icloud.com>
2024-06-18 23:08:31 +0200

    feat: obects array

---
+++ js/script.js
@@ -0,0 +1,39 @@
+//Creare l’array di oggetti con le informazioni fornite.
+const team = [
+
+    { //1
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO',
+        foto: ''
+    },
+    { //2
+        nome: 'Angela Caroll',
+        ruolo: 'Chief Editor',
+        foto: ''
+    },
+    { //3
+        nome: 'Walter Gordon',
+        ruolo: 'Office Manager',
+        foto: ''
+    },
+    { //4
+        nome: 'Angela Lopez',
+        ruolo: 'Social Media Manager',
+        foto: ''
+    },
+    { //5
+        nome: 'Scott Estrada',
+        ruolo: 'Developer',
+        foto: ''
+    },
+    { //6
+        nome: 'Barbara Ramos',
+        ruolo: 'Graphic Designer',
+        foto: ''
+    }
+]
+
+//Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto.
+
+
+//Stampare le stesse informazioni su DOM sottoforma di stringhe.
